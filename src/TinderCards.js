bcaf83a0eeb41f64fc1d5602ec94a98612ee89a3
michael <zhoumichael8@gmail.com>
2020-09-16 10:46:36 +0100

    Add Tinder Card components

---
+++ src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TinderCard.css";
+
+function TinderCards() {
+    return (
+        <div className="tinderCards">
+            
+        </div>
+    )
+}
+
+export default TinderCards
